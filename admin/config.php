7422dfa329bbc85c0f0ecf52b059bb508bee0a96
root <theingiorama@gmail.com>
2016-06-06 00:25:28 -0400

    royal shop git set up

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eduprosolutions.com/royalshop/admin/');
+define('HTTP_CATALOG', 'http://eduprosolutions.com/royalshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eduprosolutions.com/royalshop/admin/');
+define('HTTPS_CATALOG', 'http://eduprosolutions.com/royalshop/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/royalshop/admin/');
+define('DIR_SYSTEM', '/var/www/royalshop/system/');
+define('DIR_IMAGE', '/var/www/royalshop/image/');
+define('DIR_LANGUAGE', '/var/www/royalshop/admin/language/');
+define('DIR_TEMPLATE', '/var/www/royalshop/admin/view/template/');
+define('DIR_CONFIG', '/var/www/royalshop/system/config/');
+define('DIR_CACHE', '/var/www/royalshop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/royalshop/system/storage/download/');
+define('DIR_LOGS', '/var/www/royalshop/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/royalshop/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/royalshop/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/royalshop/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'kokosep26');
+define('DB_DATABASE', 'royalshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'r_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://eduprosolutions.com/royalshop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://eduprosolutions.com/royalshop/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/royalshop/catalog/');
+define('DIR_SYSTEM', '/var/www/royalshop/system/');
+define('DIR_IMAGE', '/var/www/royalshop/image/');
+define('DIR_LANGUAGE', '/var/www/royalshop/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/royalshop/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/royalshop/system/config/');
+define('DIR_CACHE', '/var/www/royalshop/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/royalshop/system/storage/download/');
+define('DIR_LOGS', '/var/www/royalshop/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/royalshop/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/royalshop/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'kokosep26');
+define('DB_DATABASE', 'royalshop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'r_');
